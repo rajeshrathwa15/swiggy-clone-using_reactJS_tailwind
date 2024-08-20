969274cf7b08d5cd185c3cc7a10b78ee67b91eb2
rajeshrathwa15 <bitsjin37@bitbaroda.com>
2024-08-20 20:42:59 +0530

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://rajeshrathwa15.github.io/swiggy-clone-using_reactJS_tailwind/', // Ensure this is correct
+  base: 'https://rajeshrathwa15.github.io/swiggy-clone-using_reactJS_tailwind/dist', // Ensure this is correct
 });
